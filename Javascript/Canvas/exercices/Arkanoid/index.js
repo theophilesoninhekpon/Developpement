25350e98037df912529ab56b24837c3cd0d168eb
Théophile Soninhekpon <tsonihekpon@gmail>
2023-06-30 19:43:43 +0200

    23

--- Javascript/Canvas/exercices/Arkanoid/index.js
+++ Javascript/Canvas/exercices/Arkanoid/index.js
@@ -15,15 +15,15 @@ let ball = {
     x: 270,
     y: 485,
     color: "red",
-    radius: 15,
-    directionY: -1,
+    radius: 10,
+    direction: {x:0, y:-1},
     speed : 10
 }
 
 // Représentation de l'espace de jeu
 let game = {
-    width: 550,
-    height: 600,
+    width: 600,
+    height: 550,
     color: "lightgray",
     gameOver: false,
     start: false,
@@ -32,14 +32,25 @@ let game = {
 
 // Représentation du plateau
 let paddle = {
-    x: 250,
-    y: 500,
+    x: 240,
+    y: 540,
     speed: 25,
     color: "blue",
-    width: 100,
-    height: 50,
+    width: 120,
+    height: 10,
     directionX: 1
 }
+
+// Représentation d'une brique
+let brick = {
+    x: 0,
+    y: 0,
+    color: "lightblue",
+    width: 50,
+    height: 20
+}
+
+let bricksCollection = [];
 
 /************************************************************************************/
 /****************************** FONCTIONS *******************************************/
@@ -47,73 +58,94 @@ let paddle = {
 
 function playGame(){
     
-    
+    // Changement de la direction Y de la balle
     if(ball.y < ball.radius ){
 
-        ball.directionY = 1;
+        ball.direction.y = 1;
         
     } else if ( ball.y > game.height - ball.radius) {
 
-        ball.directionY = -1;
+        ball.direction.y = -1;
+
+    }
+
+    if(ball.x < ball.radius ){
+
+        ball.direction.x = 1;
+        
+    } else if ( ball.x > game.width - ball.radius) {
+
+        ball.direction.x = -1;
 
     }
     
     detectCollisions();
 
+    ball.y += ball.direction.y * ball.speed;
+    console.log(ball.y)
+    ball.x += ball.direction.x * ball.speed;
+    console.log(ball.x)
     displayGame();
 
 }
 
 
+// Animation de la balle
+
 function displayGame(){
     
-    // Dessin de la balle
-        if( game.gameOver === false){
+        if( game.gameOver === false && game.start === true && game.pause === false){
 
-            context.clearRect(0,  0, 1900, 550)
+            displayPaddleAndBall();
 
-            context.strokeStyle = ball.color;
+            let numberOfBricks = 10;
+            let brickPositionX;
 
-            context.fillStyle = ball.color;
+            for( let i = 0; i < numberOfBricks; i++){
 
-            context.beginPath();
-        
-            context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-            
-            context.stroke();
-            
-            context.fill();
+                brick.x += brick.width + 10;  
+                brick.x += brick.width + 10;  
 
-            context.strokeStyle = paddle.color;
+                displayBricks();
 
-            context.fillStyle = paddle.color;
-
-            context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height)
+            }
 
             animationID = requestAnimationFrame(playGame);
 
-        } else{
+        } else if(game.start === false && game.pause === true){
+
+            cancelAnimationFrame(animationID);
+
+        } else if(game.gameOver === true){
 
             cancelAnimationFrame(animationID);
 
             context.font = "bold 30px Courier New";
 
-            context.fillText("Game over", game.width / 2 - context.measureText("Game over").width / 2, game.height /2  )
-        }
-        
+            context.fillText("Game over", (game.width - context.measureText("Game over").width) / 2, game.height /2  );
 
-    // Dessin du plateau
+        }
 
 }
 
 
-function initGame() {
+// Dessin d'une brique
 
-    canvasDom = document.getElementById("canvas");
+function displayBricks(width, height){
 
-    context = canvasDom.getContext("2d");
+    context.strokeStyle = brick.color;
 
-    // Premier affichage de la balle et du plateau
+    context.fillStyle = brick.color;
+
+    context.fillRect(width, height, brick.width, brick.height);
+
+}
+
+
+
+// Dessine la balle et le plateau
+
+function displayPaddleAndBall(){
 
     context.clearRect(0,  0, 1900, 550)
 
@@ -133,25 +165,26 @@ function initGame() {
 
     context.fillStyle = paddle.color;
 
-    context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height)
+    context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
+}
 
 
+function initGame() {
 
+    canvasDom = document.getElementById("canvas");
 
-    ballAreaWidth = ball.radius * 2;
-    ballAreaHeight = ball.radius * 2;
+    context = canvasDom.getContext("2d");
 
-    context.strokeStyle = paddle.color;
-
-    context.fillStyle = paddle.color;
-
-    context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height)
+    displayPaddleAndBall();
+    
+    displayBricks();
 
     // Gestionnaire d'évènement de l'appui sur le clavier
 
     document.addEventListener("keydown", function keyboardEvent(event){
 
-            paddle.speed = 25;
+        paddle.speed = 25;
 
         if(event.code === "ArrowRight"){
 
@@ -162,26 +195,24 @@ function initGame() {
 
             paddle.directionX = -1;
             paddle.x += paddle.directionX * paddle.speed;
+
         }
 
+        // Position de la balle au milieu du plateau au démarrage
         if(game.start === false){
 
-            ball.x = paddle.x + paddle.width / 2;
-            ball.y = paddle.y - ball.radius;
+            if(count === 0){
 
-            context.clearRect(0,  0, 1900, 550);
-            context.strokeStyle = ball.color;
-            context.fillStyle = ball.color;
-            context.beginPath();
-            context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-            context.stroke();
-            context.fill();
-            context.strokeStyle = paddle.color;
-            context.fillStyle = paddle.color;
-            context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+                ball.x = paddle.x + paddle.width / 2;
+                ball.y = paddle.y - ball.radius;
+
+                displayPaddleAndBall();
+
+            }
 
         }
         
+        // Pour restreindre le mouvement du plateau
         if(paddle.x > game.width - paddle.width){
 
             paddle.x = game.width - paddle.width;
@@ -194,56 +225,98 @@ function initGame() {
 
         }
 
+        // Détection du clic sur la touche Espace
         if(event.code === "Space"){
 
             count++;
+            
+            // Si le Game est over, on reprend au clic
+            if(game.gameOver === true){
 
-            if(count % 2 === 0){
-
+                console.log("reprise au game over")
+                initPositions();
+                game.start = false;
                 game.pause = true;
-                cancelAnimationFrame(animationID);
-
-            } else if( count % 2 === 1){
-
-                game.pause = true;
-                playGame();
+                game.gameOver = false;
+                count = 0;
+                displayPaddleAndBall();
 
             } 
-            
+
+            // Play et Pause
+            if(count % 2 === 0){
+
+                game.start = false;
+                game.pause = true;
+                // cancelAnimationFrame(animationID);
+
+            } else if( count % 2 === 1){
+    
+                game.start = true;
+                game.pause = false;
+                playGame();
+                console.log(game.gameOver)
+        
+            }
         }
 
     })
 
 }
 
-// Dectection de la collision entre la balle et le plateau
+// Détection de la collision entre la balle et le plateau
 
 function detectCollisions(){
 
-    if(ball.y === game.height - paddle.height - ball.radius && (ball.x > paddle.x && ball.x < paddle.x + paddle.width)){
+    let firstTierceOfPaddle = paddle.x + paddle.width * 1 / 3;
+    let secondTierceOfPaddle = paddle.x + paddle.width * 2 / 3;
+    let thirthTierceOfPaddle =  paddle.x + paddle.width;
 
-        ball.directionY = -1;
+    if(ball.y >= game.height - paddle.height - ball.radius && (ball.x + ball.radius > paddle.x && ball.x - ball.radius < paddle.x + paddle.width)){
+
+        console.log("balle retourne")
+        if(ball.x > paddle.x &&  ball.x < firstTierceOfPaddle){
+
+            ball.direction.x = -1;
+
+        } else if(ball.x > firstTierceOfPaddle && ball.x < secondTierceOfPaddle){
+
+            console.log(ball.x)
+            ball.direction.x = 0;
+
+        } else if(ball.x > secondTierceOfPaddle && ball.x < thirthTierceOfPaddle){
+
+            ball.direction.x = 1;
+
+        }
+
+        ball.direction.y = -1;
 
     } else if(ball.y === game.height - ball.radius){
         
         game.gameOver = true;
-        ball.directionY = 0;
+        ball.direction.y = 0;
 
     }
-
-    ball.y += ball.directionY * ball.speed;
 
     return game.gameOver;
     
 }
 
+
+// Réinitialisation de la position du plateau et de la balle au milieu
+
 function initPositions(){
+
+    paddle.x = 240;
+
+    paddle.y = 540; 
 
     ball.x = paddle.x + paddle.width / 2;
 
     ball.y = paddle.y - ball.radius;
 
-    ball.directionY = 0;
+    ball.direction.y = 0;
 
 }
 
@@ -253,7 +326,11 @@ function initPositions(){
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
+    // On réinitialise tout au début
     initPositions();
+
+    // On initialise le jeu
     initGame();
+
 
 })
